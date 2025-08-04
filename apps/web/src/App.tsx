b36796203c169603a802c28f4936bbf58f7807f2
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-08-04 22:38:04 +0700

    🔧 feat: initialize Vite + React + TypeScript

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,24 +1,35 @@
-import { APITester } from "./APITester";
-import "./index.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+function App() {
+  const [count, setCount] = useState(0)
 
-export function App() {
   return (
-    <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
       </p>
-      <APITester />
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
